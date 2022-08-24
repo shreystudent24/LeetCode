73465e3f7dd2c940d4b4f03e095d6e69c4254da8
Shrey Vardhan Dhiman <99477871+shreystudent24@users.noreply.github.com>
2022-08-24 21:21:05 +0530

    Time: 45 ms (23.90%), Space: 5.9 MB (72.69%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,11 @@
+#include<math.h>
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        
+        if(n>0 && int(pow(3,19))%n==0){
+            return true;
+        }
+        return false;
+    }
+};
