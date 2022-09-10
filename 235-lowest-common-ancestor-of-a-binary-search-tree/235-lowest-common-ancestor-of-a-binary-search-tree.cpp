9847984f4c803f1937be2b76236a9536efab237f
Shrey Vardhan Dhiman <99477871+shreystudent24@users.noreply.github.com>
2022-09-10 23:25:43 +0530

    Time: 60 ms (17.28%), Space: 23.3 MB (14.12%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,14 +15,15 @@ public:
             return root;
         }
         
-        if(root->val>p->val && root->val>q->val){
-            return lowestCommonAncestor(root->left, p, q);
+        if(root->val > p->val && root->val>q->val){
+            return lowestCommonAncestor( root->left, p,  q);
         }
-       else if(root->val<p->val && root->val<q->val){
-            return lowestCommonAncestor(root->right, p, q);
+        
+         if(root->val < p->val && root->val<q->val){
+            return lowestCommonAncestor( root->right, p,  q);
         }
-        else{
-            return root;
-        }
+        
+        return root;
     }
+    
 };
