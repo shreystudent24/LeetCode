d61c591c7a04f1b07b61afa03d7ec93c32d25e55
Shrey Vardhan Dhiman <99477871+shreystudent24@users.noreply.github.com>
2022-11-07 11:24:50 +0530

    Time: 43 ms (69.94%), Space: 19.3 MB (13.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-            //Used Stable Parition of STL! 
-         stable_partition(rbegin(nums),rend(nums),logical_not<int>());
+        /*One Liner Used Stable Parition of STL! 
+       stable_partition(rbegin(nums),rend(nums),logical_not<int>());
+       */
+        
+        //Another Solution without STL.
+        for(int lastNonZero = 0, curr = 0; curr<nums.size();curr++){
+            if(nums[curr] != 0){
+                swap(nums[lastNonZero++],nums[curr]);
+            }
+        }
     }
 };
