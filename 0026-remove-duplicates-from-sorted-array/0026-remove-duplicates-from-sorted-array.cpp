ce933664b1eeefe059c3f57f2455668fa6ca42f4
Shrey Vardhan Dhiman <99477871+shreystudent24@users.noreply.github.com>
2022-11-16 16:13:48 +0530

    Time: 15 ms (84.56%), Space: 18.5 MB (37.86%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,16 +4,16 @@ public:
         //Two Pointer Approach 
         if(nums.size() == 0) return 0;  //Base Case
          int left = 0;  //Left Pointer
-        for(int right =1 /* Right Pointer*/; right< nums.size(); right++){
-           
+        for(int right =1; right< nums.size(); right++){ //right = right pointer
+            //if nums are not equal
             if(nums[left] != nums[right]){
                left++;
-                //if nums are not equal
            }
             
             //If nums are equal
-               nums[left] = nums[right];
-           }
-             return left+1;
+             nums[left] = nums[right];
+             }
+        
+        return left+1;
     }
 };
